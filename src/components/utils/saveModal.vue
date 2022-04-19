a120e9daa7401a00f275058c0634e9479b597294
Andre Conjo <andreconjo@iMac-de-Andre.local>
2022-04-18 21:21:35 -0300

    Ajustado modal de salvar catalogo

--- src/components/utils/saveModal.vue
+++ src/components/utils/saveModal.vue
@@ -2,8 +2,13 @@
 
    <div class="modal">
        <div class="content">
-           <p>Nome para sua cena:</p>
+           <p class="header bold">Configurações:</p>
+           <div class="form">
+            <span>Nome do catálogo:</span>
             <input v-model="scene.alias" type="text">
+            <span>Conflita com quais softgoals? (separado por virgula):</span>
+            <input v-model="scene.conflicts" type="text">
+            </div>
             <button @click="salvar">salvar</button>
        </div>
     </div>
@@ -27,8 +32,9 @@ export default {
 
     methods: {
         salvar() {
-            console.log(this.scene)
-             axios
+            this.scene.conflicts = this.scene.conflicts.split(',');
+            console.log(this.scene);
+            axios
             .post('http://localhost:8080/save', this.scene)
             .then(response => { 
                 this.scenes = response.data
@@ -45,6 +51,15 @@ export default {
 
 <style>
 
+    .header {
+        margin-top: 5px;
+        font-size: 16px;
+    }
+
+    .bold {
+        font-weight: bold;
+    }
+
     .modal {
         width: 100%;
         height: 100%;
@@ -59,6 +74,22 @@ export default {
         margin: 10vh auto;
         background: white;
 
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        padding: 10px;
+    }
+
+    .form span {
+        margin-top: 5px;
+    }
+
+    .form input {
+        display: block;
+        width: 50%;
+        margin: auto;
     }
 
 </style>
